7a91b5cdab9f8bd0cbb1a968ad27c60d27bf8f5d
benediktweihs <benedikt.weihs@student.uibk.ac.at>
2021-03-15 12:59:19 +0100

    everything done

---
+++ 01/task2/lcc2/plotter.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+plt.style.use("latex.mplstyle")
+
+def numpyArr(name):
+    df = pd.read_csv(name, delimiter="\t")
+    return df.to_numpy(dtype=float).transpose()
+
+if __name__ == "__main__":
+    names=["slow", "medium", "fast"]
+    prefix=["", "../data/", "../data/new/", "../data/new2/"]
+    data0 = []
+    data1 = []
+    iterations0 = [4, 20, 50]
+    iterations1 = [10, 30, 100]
+
+    scaleX = 1
+    scaleY = 1
+    iterTot = [iterations0, iterations1, iterations1, iterations1]
+
+    fig, ax = plt.subplots(nrows=4, ncols=3, figsize=(10, 8))
+    for lccLocal, iter in enumerate(iterTot):
+        for j, n in enumerate(names):
+            #if lccLocal == 0: continue
+            dat = numpyArr(prefix[lccLocal] + n + ".txt")
+            length = len(dat)
+            if length > 2:
+                maxCol = length-1
+                threads = dat[0]
+                times = np.mean(dat[1:maxCol], axis=0)
+                err = np.std(dat[1:maxCol], axis=0) / np.sqrt(iter[j])
+                ax[lccLocal][j].errorbar(scaleX * threads, scaleY * times, yerr=scaleY * err, fmt='o',
+                             linewidth=0.8, capsize=2, capthick=0.6, markersize=0)
+            else:
+                threads = dat[0]
+                times = dat[1] / iter[j]
+            ax[lccLocal][j].plot(scaleX * threads, scaleY * times, "ko", markersize=2)
+            ax[lccLocal][j].set_xlabel("#Threads", fontsize=10)
+            ax[lccLocal][j].set_ylabel(r'$\frac{t}{s}$', fontsize=15)
+            ax[lccLocal][j].tick_params(axis='both', which='major', labelsize=8)
+            if lccLocal == 2: ax[lccLocal][j].set_title("Flag -O2")
+            if lccLocal == 3: ax[lccLocal][j].set_title("Flag -O2 -O3")
+    plt.tight_layout(pad=1.5)
+    plt.savefig("results.pdf", bbox_inches="tight")
+
+
+
--- 01/task2/profiler.sh
+++ 01/task2/profiler.sh
@@ -7,17 +7,19 @@ echo
 implementations=(slow medium fast)
 mkdir -p data
 declare -A iterations
-iterations[${implementations[0]}]=1
-iterations[${implementations[1]}]=2
-iterations[${implementations[2]}]=2
+iterations[${implementations[0]}]=10
+iterations[${implementations[1]}]=30
+iterations[${implementations[2]}]=100
 
 
 for exec in ${implementations[*]}; do
   # create/empty files
-  > ./data/$exec.txt
-  echo -e "#Threads\t Time">>./data/$exec.txt
+  > ./data/new2/$exec.txt
+  echo -n -e "#Threads\t">>./data/new2/$exec.txt
+  for (( j=1; j<=${iterations[$exec]}; j++ )); do echo -n -e "Time\t">>./data/new2/$exec.txt; done
+  echo "TotalTime">>./data/new2/$exec.txt
   echo "Profiling" $exec "implementation"
-  echo "Iterating" ${iterations[$exec]} "times for each OMP_NUM_THREADS"
+  echo "Iterating" ${iterations[$exec]} "time(s) for each OMP_NUM_THREADS"
   for i in {1..12}; do
     # write results to files
     echo -e "\n OMP_NUM_THREADS =" $i "\n \t iteration_no:"
@@ -26,15 +28,17 @@ for exec in ${implementations[*]}; do
 
     # execute each program for each value of OMP_NUM_THREADS $iterations times
     # add all times
+    echo -n -e $i "\t">>./data/new2/$exec.txt
     for (( j=1; j<=${iterations[$exec]}; j++ ))
     do
       echo -e "\t" $j
       str=$(./$exec)
       temp=$(echo $temp"+" "${str:21:-8}" | bc)
+      echo -n -e "${str:21:-8}" "\t">>./data/new2/$exec.txt
     done
 
     # write to file
-    echo -e $i "\t" $temp>>./data/$exec.txt
+    echo $temp>>./data/new2/$exec.txt
   done
 done
 
@@ -48,6 +52,6 @@ fi
 # plot results
 for exec in ${implementations[*]}; do
   echo
-  cat ./data/$exec.txt
-  cat ./data/$exec.txt | gnuplot -p -e "set xrange [0:13]; set pointsize 3; plot '-' linetype 2 pointtype 2"
+  cat ./data/new2/$exec.txt
+  cat ./data/new2/$exec.txt | gnuplot -p -e "set xrange [0:13]; set pointsize 3; plot '-' linetype 2 pointtype 2"
 done
