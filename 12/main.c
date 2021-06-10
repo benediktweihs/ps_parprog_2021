5d86560d8cc10946554bdeec29e2fca8f8cf15dd
benediktweihs <benedikt.weihs@student.uibk.ac.at>
2021-06-10 18:58:03 +0200

    ex_12

---
+++ 12/main.c
@@ -0,0 +1,12 @@
+//
+// Created by weihs on 10/06/2021.
+//
+
+#include "main.h"
+
+int main(void) {
+	printf("hallo\n");
+	printf("%ld\n", sizeof(long double));
+	printf("%d\n", simple());
+	return EXIT_SUCCESS;
+}
